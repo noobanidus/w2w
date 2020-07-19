f29cf332d700eac35ce42a59b7833867c356d5b4
Jon McManus <due@wxwhatever.com>
2020-07-19 12:43:51 +1000

    Update a bunch of things, fix bugs, etc.

--- src/main/java/noobanidus/mods/w2w/WaystonesHandler.java
+++ src/main/java/noobanidus/mods/w2w/WaystonesHandler.java
@@ -2,12 +2,17 @@ package noobanidus.mods.w2w;
 
 import net.blay09.mods.waystones.api.IWaystone;
 import net.blay09.mods.waystones.api.WaystoneActivatedEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class WaystonesHandler {
   public static void onWaystoneActivated(WaystoneActivatedEvent event) {
     if (event.getPlayer().world.isRemote()) {
-      IWaystone waystone = event.getWaystone();
-      WaypointHandler.makeWaypoint(waystone.getPos(), waystone.getName());
+      PlayerEntity player = Minecraft.getInstance().player;
+      if (event.getPlayer().equals(player)) {
+        IWaystone waystone = event.getWaystone();
+        WaypointHandler.makeWaypoint(waystone.getPos(), waystone.getName());
+      }
     }
   }
 }
